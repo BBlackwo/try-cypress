9a1d68d7a6750fdfec720174307348f1c5cc98d8
Benjamin Blackwood <ben.blackwood@auspost.com.au>
2020-11-06 14:04:50 +1100

    test: add tests and cypress dashboard

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,4 +1,26 @@
-it('loads examples', () => {
-  cy.visit('/');
-  cy.contains('Here are some links to help you get started');
+describe(`app`, () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('loads examples 1', () => {
+    cy.contains('Here are some links to help you get started');
+  });
+
+  it('loads examples 2', () => {
+    cy.contains('Here are some links to help you get started');
+  });
+
+  it('loads examples 3', () => {
+    cy.contains('Here are some links to help you get started');
+  });
+
+  it('loads examples 4', () => {
+    cy.contains('Here are some links to help you get started');
+  });
+
+  it('loads examples 5', () => {
+    cy.contains('Here are some links to help you get started');
+  });
+
 });
